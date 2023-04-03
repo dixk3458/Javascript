caa5d8400258f4cf059ec6295da08e6c9e67ac57
dixk3458 <dixk3458@naver.com>
2023-04-03 17:22:37 +0900

    naming

---
+++ 2.naming.js
@@ -0,0 +1,45 @@
+// 주석을 달면 됨
+/**
+ * 주석이됨
+ * 다음줄로 자동으로 넘어감
+ * 주석은 코드에 실행 X
+ */
+
+/**
+ * 변수 규칙!
+ * 라틴 문자 (0~9,a~z,A~Z)
+ * 대소문자를 구분한다.
+ * 추천: camelCase (단어 두개를 연결할 때에는 두번째 단어부터 대문자로 시작해준다. )
+ * 한국어 변수 이름 X
+ * 예약어 X
+ * 숫자로 시작 X
+ * 특수문자 X ( _ , $ 두가지는 예외)
+ * 이모지 X
+ * 여러개의 변수를 1,2,3 숫자로 구분 X 
+ * 최대한 의미있게 , 구체적인 이름으로 작성하자!
+ */
+
+let apple;
+
+let redApple;
+
+// 나쁜예제
+
+let number = 20;
+
+let audio1;
+let audio2;
+
+// 좋은예제
+
+let myAge = 20;
+
+let backgroundAudio;
+let windAudio;
+
+// 꿀팁!
+
+let audioBackground;
+let audioWind;
+
+audioBackground;
