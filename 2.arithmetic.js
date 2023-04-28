365622c71913a7b18da5dcedb7e10b618b3f4985
dixk3458 <dixk3458@naver.com>
2023-04-28 17:43:04 +0900

    산술 연산자

---
+++ 2.arithmetic.js
@@ -0,0 +1,22 @@
+// 산술 연산자 (Arithmetic operators)
+// + 더하기
+// - 빼기
+// * 곱하기
+// / 나누기
+// % 나머지
+// ** 지수 연산자
+
+console.log(5 + 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2);
+console.log(5 ** 3);
+console.log(Math.pow(5,3));
+
+// 주의점!
+let text = "두개의" + "문자를 연결해준다.";
+console.log(text);
+
+// 숫자와 문자를 더하면 문자열로 반환된다.
+text = "20"+20; 
+console.log(text); 
