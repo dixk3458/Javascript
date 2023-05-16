a0220aeeabdeaeb7afb12097002b85ade7e305e1
dixk3458 <dixk3458@naver.com>
2023-05-17 00:29:35 +0900

    삼항조건 연산자

---
+++ 3.조건문/2.ternary.js
@@ -0,0 +1,12 @@
+// 삼항 조건 연산자 Ternary Operator
+// 조건식 ? 참인경우 : 거짓인경우;
+
+let name = '정재웅';
+
+name === '정재웅' ? console.log('정답입니다!') : console.log('틀렸습니다.');
+
+let fruit = 'apple';
+// 삼항조건 연산자를 이용해서 할당도 할수있다.about__majors
+// 즉 조건에 따라 할당값을 다르게 해준다.
+let emoji = fruit === 'apple' ? '🍎' : '❌';
+console.log(emoji);
