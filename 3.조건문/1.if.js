0139a0956dd7b1beb98b6e14d0545cb1d6ace35e
dixk3458 <dixk3458@naver.com>
2023-05-17 00:22:56 +0900

    if문 연습

---
+++ 3.조건문/1.if.js
@@ -0,0 +1,17 @@
+// 조건문 Conditinal Statement
+// if(조건){ }
+// if(조건){ } else { }
+// if(조건1) { } else if(조건2) { } else {}
+
+let fruit = 'orange';
+
+if (fruit === 'apple') {
+  console.log('🍎');
+} else if (fruit === 'orange') {
+  console.log('🍊');
+} else {
+  console.log('❓');
+}
+
+
+// 조건에 false 값을 주면 코드블럭을 실행 안하게 한다.
