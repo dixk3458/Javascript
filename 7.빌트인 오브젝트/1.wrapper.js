eae0165be0e22f4fe8222654453fa80c379f69a1
dixk3458 <dixk3458@naver.com>
2023-06-25 22:12:17 +0900

    래퍼 객체 개념정리

---
+++ 7.빌트인 오브젝트/1.wrapper.js
@@ -0,0 +1,15 @@
+const number = 123;
+
+// 원시타입 number
+console.log(number);
+
+// Number 클래스로 감싸진 원시타입 즉 빌트인 객체
+console.log(number.toString());
+
+// 즉 자바스크립트에서 원시타입은 필요한 순간에 빌트인 오브젝트로 바꿀수있다.
+
+/**
+ * ex)
+ * String.trim()  : 문자열 양 끝의 공백을 제거한다.(새로운 문자열 반환)
+ * Number.toString() : 숫잡타입 데이터를 문자열 타입으로 return 해준다.
+ */
