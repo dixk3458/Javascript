0e4e89890366293ffa07fbea12d303d42ecdede3
dixk3458 <dixk3458@naver.com>
2023-06-26 16:40:52 +0900

    수학관련 함수들

---
+++ 7.빌트인 오브젝트/5.math.js
@@ -0,0 +1,38 @@
+// Math
+// static properties, method
+
+// 자연로그
+console.log(Math.E);
+
+// 원주율
+console.log(Math.PI);
+
+// 절대값
+console.log(Math.abs(123, 4));
+console.log(Math.abs(-20));
+
+// 소수점 이하를 올림
+console.log(Math.ceil(1.4));
+// 소수점 이하를 내림
+console.log(Math.floor(1.4));
+
+// 소수점 이하를 반올림
+console.log(Math.round(1.4));
+console.log(Math.round(1.5));
+
+// 0~1사이의 랜덤한 값을 랜덤한 값을 반환해준다.
+console.log(Math.random());
+// 0~10사이의 랜덤한 값을 출력
+console.log(Math.floor(Math.random() * 10 + 1));
+
+// 제곱
+console.log(3 ** 2);
+console.log(Math.pow(3, 2));
+
+// 제곱근
+console.log(Math.sqrt(9));
+
+// 주어진 값중에 최대 , 최소 찾기
+console.log(Math.max(1, 2, 3, 5, 2));
+
+console.log(Math.min(1, 2, 4));
