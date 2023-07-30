ae283296162999a4da72a7c0e852f2519d9c4acd
Jaewoong <dixk3458@naver.com>
2023-07-30 23:08:45 +0900

    주석

---
+++ 12.주석/4.counter.js
@@ -0,0 +1,8 @@
+let count = 0;
+export function increase() {
+  count++;
+  console.log(count);
+}
+
+console.log(count);
+increase();
---
+++ 12.주석/4.main.js
@@ -0,0 +1,5 @@
+// import increase from './4.counter.js';
+// import { increase as increase1 } from './4.counter.js';
+import * as counter from './4.counter.js';
+
+counter.increase();
